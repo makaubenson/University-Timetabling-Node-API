70e885bce5710a21fb99d56ce49bd70e30863485
benson <bensonmakau2000@gmail.com>
2023-01-21 21:33:16 +0300

    deleted unwanted comments

--- app.js
+++ app.js
@@ -1,34 +1,24 @@
-const path = require('path');
-const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const cors = require('cors');
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
 
-const userRouter = require('./routes/userRouter');
+const userRouter = require("./routes/userRouter");
 
 const app = express();
 
 //implement CORS: allows CORS for all incoming requests to our API.
 app.use(cors());
-//Access-Control-Allow-Origin
-//if we wanted to not have CORS for all requests 
-// however our backend and frontend exist on different urls
-//backend: api.natours.com    front-end:natours.com
-// app.use(cors({
-//   origin:'https://www.natours.com'
-// }));
 
-app.options('*', cors());//all resources
-// app.options('/api/v1/tours/:id', cors());//single resource
+app.options("*", cors()); //all resources
 
 // Development logging
-if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
-  }
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
-
-  // 3) ROUTES
-app.use('/', userRouter);
-
+// 3) ROUTES
+app.use("/", userRouter);
 
 module.exports = app;
