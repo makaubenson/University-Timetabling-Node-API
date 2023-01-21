9949d0ae7dd653ca6fdd4ef6c9c3ba808cc038e1
benson <bensonmakau2000@gmail.com>
2023-01-21 23:57:26 +0300

    installed node packages

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
+//Require modules/files
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-
-process.on('uncaughtException', err => {
-  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
   console.log(err.name, err.message);
   process.exit(1);
 });
 
+//Load Environment Configs
+dotenv.config({ path: "./config.env" });
 
-//Load Env settings
-dotenv.config({ path: './config.env' });
+//require app.js
+const app = require("./app");
 
-//app.js
-const app = require('./app');
-
-
+// Run this system on port 3000
 const port = process.env.PORT || 3000;
+
+//start server
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
