06c62b22074fd56a319121d1874cfb7b4a084dc3
benson <bensonmakau2000@gmail.com>
2023-01-22 15:43:21 +0300

    Working on authController
    configuring email.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,3 +3,38 @@ const jwt = require("jsonwebtoken");
 
 //Import User Model
 const User = require("./../models/userModel");
+
+//Utils
+const catchAsync = require("./../utils/catchAsync");
+const AppError = require("./../utils/appError");
+
+//create token
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
+const createSendToken = (user, statusCode, req, res) => {
+  const token = signToken(user._id);
+
+  res.cookie("jwt", token, {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+    secure: req.secure || req.headers["x-forwarded-proto"] === "https",
+  });
+
+  //   The user's password property is then set to undefined to remove it from the output
+  // before sending the token, user object and success status in the response.
+  user.password = undefined;
+
+  res.status(statusCode).json({
+    status: "success",
+    token,
+    data: {
+      user,
+    },
+  });
+};
---
+++ utils/email.js
@@ -0,0 +1,66 @@
+const nodemailer = require("nodemailer");
+const { htmlToText } = require("html-to-text");
+
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email; //recipient
+    this.firstName = user.name.split(" ")[0]; //recipient firstname
+    this.url = url;
+    this.from = `${process.env.EMAIL_SENDER_NAME} <${process.env.EMAIL_FROM}>`; //sender name and email
+  }
+
+  newTransport() {
+    if (process.env.NODE_ENV === "production") {
+      // Sendgrid
+      return nodemailer.createTransport({
+        service: "SendGrid",
+        auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD,
+        },
+      });
+    }
+
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  // Send the actual email
+  async send(template, subject) {
+    // 1) Render HTML based on a pug template
+    const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+      firstName: this.firstName,
+      url: this.url,
+      subject,
+    });
+
+    // 2) Define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.fromString(html),
+    };
+
+    // 3) Create a transport and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send("Welcome onboard");
+  }
+
+  async sendPasswordReset() {
+    await this.send(
+      "passwordReset",
+      "Your password reset token (valid for only 10 minutes)"
+    );
+  }
+};
